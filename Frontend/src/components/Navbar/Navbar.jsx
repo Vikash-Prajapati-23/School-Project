f60f1b0fac89eecadb0583d8219e9b99aad2ef3e
Vikash-Prajapati-23 <122448220+Vikash-Prajapati-23@users.noreply.github.com>
2025-04-05 23:00:42 +0530

    Log in ans Sign up Created.

--- Frontend/src/components/Navbar/Navbar.jsx
+++ Frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,21 @@
 import React, { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { SiGmail } from "react-icons/si";
+
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { Loader2 } from "lucide-react";
+import { toast } from "sonner";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import Input_ from "postcss/lib/input";
+import { Separator } from "@radix-ui/react-separator";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -25,32 +42,50 @@ function Navbar() {
             } md:flex flex-col md:flex-row font-bold items-center space-y-3 md:space-y-0 md:mb-0 mb-3 md:space-x-5 md:text-1xl `}
           >
             <li>
-              <a href="/" className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600">
+              <a
+                href="/"
+                className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600"
+              >
                 Home
               </a>
             </li>
             <li>
-              <a href="/about" className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600">
+              <a
+                href="/about"
+                className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600"
+              >
                 About
               </a>
             </li>
             <li>
-              <a href="/Classes" className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600">
+              <a
+                href="/Classes"
+                className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600"
+              >
                 Classes
               </a>
             </li>
             <li>
-              <a href="/Teachers" className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600">
+              <a
+                href="/Teachers"
+                className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600"
+              >
                 Teachers
               </a>
             </li>
             <li>
-              <a href="/Contact" className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600">
+              <a
+                href="/Contact"
+                className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600"
+              >
                 Contact Us
               </a>
             </li>
             <li>
-              <a href="/addmission" className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600">
+              <a
+                href="/addmission"
+                className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600"
+              >
                 Addmission
               </a>
             </li>
@@ -62,17 +97,142 @@ function Navbar() {
             } md:flex flex-col md:flex-row font-bold items-center transition ease-in-out delay-150 space-y-3 md:space-y-0 md:space-x-5 md:text-1xl`}
           >
             <li>
-              <a href="/login" className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600">
-                Log in
-              </a>
+              <div>
+                <Dialog>
+                  <DialogTrigger>Log In</DialogTrigger>
+                  <DialogContent className="w-[400px]">
+                    <DialogHeader>
+                      <DialogTitle className="text-center text-base font-bold">
+                        Log In
+                      </DialogTitle>
+                      <DialogDescription className="text-center text-xs">
+                        Welcome! Please fill in the details to get started.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="mx-auto space-y-3">
+                      <div className="">
+                        <label className="text-xs">Email</label>
+                        <Input
+                          Input
+                          required={true}
+                          name="email"
+                          type="email"
+                          className="w-80"
+                        />
+                      </div>
+                      <div className="">
+                        <label className="text-xs">Password</label>
+                        <Input
+                          Input
+                          required={true}
+                          name="password"
+                          type="password"
+                          className="w-80"
+                        />
+                      </div>
+                    </div>
+                    <Button className="mx-auto w-80 mt-5">Sign Up</Button>
+
+                    <div className="flex justify-center items-center gap-2 my-3">
+                      <Separator className="w-36 h-[0.5px] bg-blue-500" />
+                      <span className="text-xs">Or</span>
+                      <Separator className="w-36 h-[0.5px] bg-blue-500" />
+                    </div>
+                    <div className="flex justify-around items-center ">
+                      <Button
+                        variant="outline"
+                        className="flex items-center gap-2 w-36"
+                      >
+                        <FcGoogle size={20} />
+                      </Button>
+
+                      <Button
+                        variant="outline"
+                        className="flex items-center gap-2 w-36"
+                      >
+                        <SiGmail size={20} className="text-red-500" />
+                      </Button>
+                    </div>
+                  </DialogContent>
+                </Dialog>
+              </div>
             </li>
             <li>
-              <a href="/signin" className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600">
-                Sign in
-              </a>
+              <div>
+                <Dialog>
+                  <DialogTrigger>Sign In</DialogTrigger>
+                  <DialogContent className="w-[400px]">
+                    <DialogHeader>
+                      <DialogTitle className="text-center text-base font-bold">
+                        Create your account
+                      </DialogTitle>
+                      <DialogDescription className="text-center text-xs">
+                        Please fill in the details to get started.
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className="mx-auto space-y-3">
+                      <div className="">
+                        <label className="text-xs">Name</label>
+                        <Input
+                          Input
+                          required={true}
+                          name="name"
+                          type="text"
+                          className="w-80"
+                        />
+                      </div>
+                      <div className="">
+                        <label className="text-xs">Email</label>
+                        <Input
+                          Input
+                          required={true}
+                          name="email"
+                          type="email"
+                          className="w-80"
+                        />
+                      </div>
+                      <div className="">
+                        <label className="text-xs">Password</label>
+                        <Input
+                          Input
+                          required={true}
+                          name="password"
+                          type="password"
+                          className="w-80"
+                        />
+                      </div>
+                    </div>
+                    <Button className="mx-auto w-80 mt-5">Sign Up</Button>
+
+                    <div className="flex justify-center items-center gap-2 my-3">
+                      <Separator className="w-36 h-[0.5px] bg-blue-500" />
+                      <span className="text-xs">Or</span>
+                      <Separator className="w-36 h-[0.5px] bg-blue-500" />
+                    </div>
+                    <div className="flex justify-around items-center ">
+                      <Button
+                        variant="outline"
+                        className="flex items-center gap-2 w-36"
+                      >
+                        <FcGoogle size={20} />
+                      </Button>
+
+                      <Button
+                        variant="outline"
+                        className="flex items-center gap-2 w-36"
+                      >
+                        <SiGmail size={20} className="text-red-500" />
+                      </Button>
+                    </div>
+                  </DialogContent>
+                </Dialog>
+              </div>
             </li>
             <li>
-              <a href="/dashboard" className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600">
+              <a
+                href="/dashboard"
+                className="hover:text-slate-600 border-b-2 border-transparent hover:border-slate-600"
+              >
                 Dashboard
               </a>
             </li>
--- Frontend/src/components/SignIn/SignIn.jsx
+++ Frontend/src/components/SignIn/SignIn.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 
 function SignIn() {
   return (
     <div>
-      Sign In
+
+<div className="col-span-1">
+          <label htmlFor="select_class">Select Class</label>
+          <select
+            id="select_class"
+            name="selectClass"
+            type="text"
+            required={true}
+            className="border border-gray-300 rounded-md p-1 h-[35px] w-full"
+          >
+            <option value="" disabled>
+              Select Class
+            </option>
+            <option value="nursery">Nursery</option>
+            <option value="class_1">Class 1</option>
+            <option value="class_2">Class 2</option>
+            <option value="class_3">Class 3</option>
+            <option value="class_4">Class 4</option>
+            <option value="class_5">Class 5</option>
+          </select>
+        </div>
+      <div>
+        <label htmlFor="role">Student</label>
+        <select name="role" id="role">
+          <option value="student">Student</option>
+          <option value="admin">Admin</option>
+          <option value="teacher">Teacher</option>
+        </select>
+      </div>
+
+      <Dialog>
+        <DialogTrigger>Open</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Are you absolutely sure?</DialogTitle>
+            <DialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
     </div>
-  )
+  );
 }
 
-export default SignIn
+export default SignIn;
